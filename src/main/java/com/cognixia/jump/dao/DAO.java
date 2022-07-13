9d81480ae3576db1de2d86bee0ca30ba761c9800
spb71 <shem.baijoo@gmail.com>
2022-07-13 14:43:10 -0400

    updated CRUD operations

--- src/main/java/com/cognixia/jump/dao/DAO.java
+++ src/main/java/com/cognixia/jump/dao/DAO.java
@@ -1,1 +1,19 @@
 package com.cognixia.jump.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface DAO<T extends Serializable> {
+    // get one by id
+    T findbyId(long id);
+
+    // get all
+    List<T> findAll();
+
+    // add to db
+    boolean create(T entity);
+
+    boolean update(T entity);
+
+    boolean deleteById(long id);
+}
--- src/main/java/com/cognixia/jump/model/User.java
+++ src/main/java/com/cognixia/jump/model/User.java
@@ -1,6 +1,8 @@
 package com.cognixia.jump.model;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
 
     private int userId;
     private String firstName;
