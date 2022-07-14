3919676280bd217153b0ec0de5bced334da50b9d
spb71 <shem.baijoo@gmail.com>
2022-07-14 17:02:08 -0400

    Updated project

--- src/main/java/com/cognixia/jump/dao/DAODriver.java
+++ src/main/java/com/cognixia/jump/dao/DAODriver.java
@@ -4,33 +4,38 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
 
+import com.cognixia.jump.exception.PWLimitException;
 import com.cognixia.jump.menu.LoginServlet;
 import com.cognixia.jump.model.ShowsWatched;
 import com.cognixia.jump.model.User;
 
 public class DAODriver extends LoginServlet {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PWLimitException {
         UserDAO userDAO = new UserDAO();
         SWDAO swDAO = new SWDAO();
 
         Scanner input = new Scanner(System.in);
 
-        
-        
-       
         // System.out.println("Enter Login Information: ");
         System.out.println("Username: ");
-        
+
         User userTest = userDAO.findbyUser(input.nextLine());
-       
+
         System.out.println(userTest);
-       
 
         if (userTest.getUserId() == (0)) {
             System.out.println("User Not Found.");
         } else {
             System.out.println("Password: ");
+            try {
+                if (userTest.getPassword().length() < 4) {
+                    throw new PWLimitException("Password is not at least 4 characters long");
+                }
+            } catch (PWLimitException e) {
+                e.getMessage();
+            }
+
             if (userTest.getPassword().equals(input.nextLine())) {
                 System.out.println("Confirmed");
                 try {
@@ -42,10 +47,8 @@ public class DAODriver extends LoginServlet {
                         System.out.println("2. Update Episode Count");
                         System.out.println("Choose Option:");
                         selection = input.nextInt();
-                                                
 
-                        switch (selection)
-                        {
+                        switch (selection) {
                             case 1:
                                 System.out.println("View WatchList.");
                                 ShowsWatched showTest = swDAO.findbyId(input.nextInt());
@@ -72,21 +75,6 @@ public class DAODriver extends LoginServlet {
                     System.out.println("message");
                 }
             }
-
-            else {
-                System.out.println("Denied");
-            }
         }
-
-        // System.out.println(userDAO.findbyPassword(input.nextLine()));
-
-        // System.out.println("Enter user_id: ");
-        // System.out.println(userDAO.findbyId(input.nextInt()));
-
-        // I'm thinking we can have it to when we enter the userid, it will return the
-        // showsWatched table and possibly plus
-        // other records such as name and username
-
     }
-
 }
