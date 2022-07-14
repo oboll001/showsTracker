3f4e232bd69b1646a6fc205ce3ca426ee0f2f2d6
spb71 <shem.baijoo@gmail.com>
2022-07-14 11:04:16 -0400

    Added exceptions for Login information

--- src/main/java/com/cognixia/jump/menu/LoginServlet.java
+++ src/main/java/com/cognixia/jump/menu/LoginServlet.java
@@ -3,10 +3,13 @@ package com.cognixia.jump.menu;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.security.auth.login.LoginException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.cognixia.jump.exception.PWLimitException;
 
 public class LoginServlet extends HttpServlet {
 
@@ -14,10 +17,26 @@ public class LoginServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
 
-        String username = req.getParameter("user_name").trim().toLowerCase();
-        String password = req.getParameter("password").trim();
+        try {
+            String username = req.getParameter("user_name").trim().toLowerCase();
+            String password = req.getParameter("password").trim();
 
-        PrintWriter pw = res.getWriter();
+            PrintWriter pw = res.getWriter();
+
+            if (password.length() < 4) {
+                throw new PWLimitException("Password length is too small.");
+            } else if (username.isEmpty() && password.isEmpty()) {
+                throw new LoginException("Must enter username and password");
+            } else {
+                System.out.println("Good password!!");
+            }
+        } catch (PWLimitException e) {
+            e.printStackTrace();
+        } catch (LoginException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
